384692cf8c05aacd3623790f9c1d0c389fb839e8
nikala <mikeladzenikala@gmail.com>
2021-01-25 00:48:54 +0400

    Succesful deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+const marked = require('marked');
+
+marked.setOptions({
+  breaks: true,
+});
+
+const renderer = new marked.Renderer();
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  
+  const [text, setText] = React.useState(`# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
 }
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+    - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+`);
+
+  return (
+  <div className="text-center px-4">
+      <h1 className="header1">Editor</h1>
+
+      <textarea 
+      name="text" 
+      id="text"
+      rows="10"
+      value={text}
+      onChange={ (e) => setText(e.target.value) }
+      className="textarea"
+      ></textarea>
+
+      <h3 className="mt-3 header2">Preview</h3>
+      <Preview markdown={text} />
+
+
+  </div>
+    );
+  }
+
+  function Preview({ markdown }) {
+    return (
+      <div
+      dangerouslySetInnerHTML={{
+        __html: marked(markdown, {renderer: renderer}),
+      }}
+      id="preview"
+      ></div>
+    );
+
+  }
+
+
+
 
 export default App;
